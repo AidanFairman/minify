ef47fe4e1b8aded9b6e097a334a780fc2abf99ae
AidanFairman <aidantfairman@gmail.com>
2021-03-06 17:03:42 +0000

    updates

---
+++ API/Controllers/MiniUrlController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MiniUrlController : ControllerBase
+    {
+        public MiniUrlController()
+        {
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            
+        }
+    }
+}
